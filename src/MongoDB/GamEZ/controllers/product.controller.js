7cb42ee7de99d2fe2af51876922fe0a221a640e5
StiviLink <minalasteve47@gmail.com>
2025-02-05 16:40:00 +0100

    Update controllers

--- src/MongoDB/GamEZ/controllers/product.controller.js
+++ src/MongoDB/GamEZ/controllers/product.controller.js
@@ -2,10 +2,14 @@ import { Product } from '../models/product.model.js'
 
 export const createProduct = async (req, res) => {
     const { serialNumber: product_serial_number, sku: product_sku, name: product_name, description: product_description
-        , seller : { name: seller_name, createdAt: seller_creation_date}, price } = req.body
+        , supplierName: seller_name, supplierCreatedAt: seller_creation_date, price } = req.body
     const category = req.body['category']?.toLowerCase()
     // Verify if category is valid
     if (['jeu vidéo','jeu de société'].includes(category)){
+        // Verify if product exists
+        const productFound = await Product.find({product: {product_serial_number}})
+        if (productFound)
+            return res.status(400).json({ message: `A product with serial number ${product_serial_number} exists.`, product: productFound})
         // Create new product
         const product = await Product.create({
             product : {product_sku, product_serial_number, product_name, product_description
@@ -19,11 +23,11 @@ export const createProduct = async (req, res) => {
 }
 
 export const getAllProducts = async (_req, res) => {
-    const menus = await Product.find()
-    res.status(200).json(menus)
+    const products = await Product.find()
+    res.status(200).json(products)
 }
 
 export const getAvailableProducts = async (_req, res) => {
-    const menus = await Product.find({ product_status: 'available' })
-    res.status(200).json(menus)
+    const products = await Product.find({ product: {product_status: 'available'} })
+    res.status(200).json(products)
 }
--- src/MongoDB/MEDIDONC/controllers/product.controller.js
+++ src/MongoDB/MEDIDONC/controllers/product.controller.js
@@ -1,10 +1,14 @@
 import { Product } from '../models/product.model.js'
 
 export const createProduct = async (req, res) => {
-    const { sku: p_sku, name: p_name, description: p_description, updatedAt: p_last_update, seller : { id
-        , name, created_at: creation_date}, price: p_price, serialNumber: p_serial_number, category } = req.body
+    const { sku: p_sku, name: p_name, description: p_description, updatedAt: p_last_update, supplierId: id
+        , supplierName: name, supplierCreatedAt: creation_date, price: p_price, serialNumber: p_serial_number, category } = req.body
     // Verify if category is valid
     if (['santé','sport sain'].includes(category?.toLowerCase())){
+        // Verify if product exists
+        const productFound = await Product.find({p_serial_number})
+        if (productFound)
+            return res.status(400).json({ message: `A product with serial number ${p_serial_number} exists.`, product: productFound})
         // Create new product
         const product = await Product.create({p_sku, p_name, p_description, p_price, p_last_update
             , p_serial_number, p_seller: {id, name, creation_date}})

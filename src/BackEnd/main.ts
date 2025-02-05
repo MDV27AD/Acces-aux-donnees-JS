749fa0f96a18c246b512093e5d433456a8ae16ec
Lucien <lucien.neuhoff@protonmail.com>
2025-02-05 16:04:30 +0100

    updated cors

--- src/BackEnd/main.ts
+++ src/BackEnd/main.ts
@@ -3,12 +3,11 @@ import { AppModule } from "./modules/app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: [/localhost:8080/] },
+  });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: [/localhost:8080/],
-  });
 
   await app.listen(process.env.PORT ?? 3000);
 }

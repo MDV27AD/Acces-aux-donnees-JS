0c6165662aa65f01e5f214b67f2d77c46c8bacac
FrozenCell239 <val8ioks@gmail.com>
2025-02-06 15:02:04 +0100

    Database addition - Added the creation script for SPORT SALUT distributor's user and its procedure for getting its products list.

---
+++ src/SQL/sportsalut.sql
@@ -0,0 +1,23 @@
+USE `central`;
+
+-- Stored procedure
+DELIMITER $$
+CREATE PROCEDURE `get_products_for_sportsalut`()
+NOT DETERMINISTIC
+READS SQL DATA
+SQL SECURITY DEFINER
+BEGIN
+    DECLARE category_id INT UNSIGNED;
+
+    -- Getting the ID of the allowed categories for the concerned distributor
+    SELECT `id` INTO category_id FROM `central`.`category` WHERE `name` = 'Sport' LIMIT 1;
+
+    -- Getting the products list
+    SELECT * FROM `central`.`product` WHERE `id_category` = category_id;
+END $$
+DELIMITER ;
+
+-- User
+CREATE USER 'sportsalut_user'@'localhost' IDENTIFIED BY 'ssuNB_SRGHA44WX£%§_5678IOùm:LK?NIBGYO8-rod(tyrfo-)';
+GRANT EXECUTE ON PROCEDURE `central`.`get_products_for_sportsalut` TO 'sportsalut_user'@'localhost';
+FLUSH PRIVILEGES;

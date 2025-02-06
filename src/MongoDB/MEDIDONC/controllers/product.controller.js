75766475d7d7ff8ab6f50bdeae5a9ea8bb4f9aa1
StiviLink <minalasteve47@gmail.com>
2025-02-06 16:13:34 +0100

    Add update for product

--- src/MongoDB/MEDIDONC/controllers/product.controller.js
+++ src/MongoDB/MEDIDONC/controllers/product.controller.js
@@ -2,18 +2,31 @@ import { Product } from '../models/product.model.js'
 
 export const createProduct = async (req, res) => {
     const { sku: p_sku, name: p_name, description: p_description, updatedAt: p_last_update, supplierId: id
-        , supplierName: name, supplierCreatedAt: creation_date, price: p_price, serialNumber: p_serial_number, category } = req.body
+        , supplierName: name, supplierCreatedAt: creation_date, price: p_price, serialNumber: p_serial_number
+        , category, update, stocked } = req.body
     // Verify if category is valid
-    if (['santé','sport sain', 'médical'].includes(category?.toLowerCase())){
+    if (['santé','sport sain'].includes(category?.toLowerCase())){
+        const data = {p_sku, p_name, p_description, p_price, p_last_update, p_serial_number
+            , p_seller: {id, name, creation_date}, p_status: stocked && 'En stock' || 'Rupture de stock'}
         // Verify if product exists
         const productFound = await Product.findOne({p_serial_number})
-        if (productFound)
-            return res.status(400).json({ message: `A product with serial number ${p_serial_number} exists.`, product: productFound})
+        if (productFound) {
+            if (update) {
+                for (const dataKey in data) {
+                    productFound[dataKey] = data[dataKey]
+                }
+                // Return the updated product
+                return res.status(201).json({message: 'Product updated successfully', product: await productFound.save()})
+            }
+            return res.status(400).json({
+                message: `A product with serial number ${p_serial_number} exists.`,
+                product: productFound
+            })
+        }
         // Create new product
-        const product = await Product.create({p_sku, p_name, p_description, p_price, p_last_update
-            , p_serial_number, p_seller: {id, name, creation_date}})
+        const newProduct = await new Product(data)
         // Return the newly created product
-        return res.status(201).json(product)
+        return res.status(201).json({message: 'Product created successfully', product: await newProduct.save()})
     }
     return res.status(400).json({ message: 'Invalid category' })
 }
--- src/MongoDB/SPORT_SALUT/controllers/product.controller.js
+++ src/MongoDB/SPORT_SALUT/controllers/product.controller.js
@@ -2,18 +2,30 @@ import { Product } from '../models/product.model.js'
 
 export const createProduct = async (req, res) => {
     const { sku: sku_produit, name: nom_produit, description: description_produit, serialNumber: serial_number_produit
-        , sellerName: nom_fournisseur, price, category } = req.body
+        , sellerName: nom_fournisseur, price, category, update, stocked } = req.body
     // Verify if the category is sport
-    if (category.includes('sport')){
+    if (category?.toLowerCase()?.includes('sport')){
+        const data = { sku_produit, serial_number_produit, nom_produit, description_produit, nom_fournisseur
+            , prix: parseFloat(price) + (price * 20 / 100), en_stock: stocked && 'Oui' || 'Non' }
         // Verify if product exists
         const productFound = await Product.findOne({serial_number_produit})
-        if (productFound)
-            return res.status(400).json({ message: `A product with serial number ${serial_number_produit} exists.`, product: productFound})
+        if (productFound) {
+            if (update) {
+                for (const dataKey in data) {
+                    productFound[dataKey] = data[dataKey]
+                }
+                // Return the updated product
+                return res.status(201).json({message: 'Product updated successfully', product: await productFound.save()})
+            }
+            return res.status(400).json({
+                message: `A product with serial number ${serial_number_produit} exists.`,
+                product: productFound
+            })
+        }
         // Create new product
-        const product = await Product.create({ sku_produit, serial_number_produit, nom_produit
-            , description_produit, nom_fournisseur, prix: parseFloat(price) + (price * 20 / 100) })
+        const newProduct = await new Product(data)
         // Return the newly created product
-        return res.status(201).json(product)
+        return res.status(201).json({message: 'Product created successfully', product: await newProduct.save()})
     }
     return res.status(400).json({ message: 'The category is not sport' })
 }

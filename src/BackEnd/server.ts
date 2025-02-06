7e755f69efc59aff6a057bc1d50f3a9fb25db5f6
Lucien <lucien.neuhoff@protonmail.com>
2025-02-06 14:13:45 +0100

    fix: cors

--- src/BackEnd/server.ts
+++ src/BackEnd/server.ts
@@ -5,9 +5,18 @@ import Express from "express";
 import { createPool } from "mysql2/promise";
 import { getDatabaseConfig } from "./database";
 import modules from "./modules";
+import cors from "cors";
 
 const port = process.env.BACKEND_PORT || 3000;
 const app = Express();
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // For legacy browser support
+    credentials: true,
+    maxAge: 3600,
+  })
+);
 
 function run() {
   const pool = createPool(getDatabaseConfig("app"));
